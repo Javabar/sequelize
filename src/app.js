2a919755c494bac3f972f29f38bdbe336f6b7929
Abdelbar Benaida <abdelbar94@hotmail.co.uk>
2022-07-04 11:30:42 +0100

    edited

--- src/app.js
+++ src/app.js
@@ -1,19 +1,25 @@
 const yargs = require("yargs");
 const {sequelize} = require("./db/connection");
-const {addMovie} = require("./movie/functions")
+const {addMovie} = require("./movie/functions");
+const { addMovie, listMovie, updateMovie, deleteMovie} = require("./movie/functions")
 
 const app = async (yargsObj) => {
     try {
         await sequelize.sync({alter: true});
         if (yargsObj.add) {
             // add something to movie table
-            await addMovie({title: yargsObj.title, actor: yargsObj.actor})
+            await addMovie({ title: yargsObj.title, actor:yargsObj.actor});
+                console.log("sucess");
         } else if (yargsObj.list) {
             // list content of movie table
+            await listMovie()
         } else if (yargsObj.update) {
             // update one entry in movie table
+            await updateMovie({ title: yargsObj.newTitle, actor: yargsObj.newActor}),
+            {where: {title: yargsObj.oldTitle}}
         } else if (yargsObj.delete) {
             // delete entry from movie
+            await deleteMovie({ title: yargsObj.title, actor: yargsObj.actor})
         } else {
             console.log("incorrect command");
         }
--- src/movie/functions.js
+++ src/movie/functions.js
@@ -8,3 +8,30 @@ exports.addMovie = async (movieObj) => {
         console.log(error)
     }
 };
+
+exports.listMovie = async () => {
+    try {
+        const selected = await Movie.findAll();
+        console.log("all movies:", JSON.stringify(selected, null, 2));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.updateMovie = async () => {
+    try {
+        const response = await Movie.update(newTitle, oldTitle);
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.deleteMovie = async (movieObj) => {
+    try {
+        const response = await Movie.destroy({ where: movieObj});
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
